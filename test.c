7f9f94fe02ff78adac80008e39caed73205f60e9
nobuyo <world_fantasy_2017@live.jp>
2017-06-20 10:47:25 +0900

    create test.c

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+    int nsize;
+    int myrank;
+    int my_name_len;
+    char my_name[MPI_MAX_PROCESSOR_NAME];
+
+    // some processes here
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Get_processor_name(my_name, &my_name_len);
+    MPI_Finalize();
+
+    return 0;
+}

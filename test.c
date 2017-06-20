fc58e93dc2efc5c29b63952e39e8d6bf177d47dc
nobuyo <world_fantasy_2017@live.jp>
2017-06-20 11:52:13 +0900

    fix test.c

--- test.c
+++ test.c
@@ -6,12 +6,13 @@ int main(int argc, char **argv) {
     int my_name_len;
     char my_name[MPI_MAX_PROCESSOR_NAME];
 
-    // some processes here
-
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     MPI_Get_processor_name(my_name, &my_name_len);
+
+    // some processes here
+
     MPI_Finalize();
 
     return 0;

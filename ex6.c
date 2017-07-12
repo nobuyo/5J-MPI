97ead84d78cb36ce884989a6bb2bfc4f92061085
nobuyo <world_fantasy_2017@live.jp>
2017-07-12 15:55:30 +0900

    fix lifegame

--- ex6.c
+++ ex6.c
@@ -17,24 +17,19 @@ int main(int argc, char **argv) {
     MPI_Get_processor_name(my_name, &my_name_len);
 
     int i, j;
-    int n, m;
+    int k, l, n, m;
     int count;
     int era = 0;
     int field[FIELD_SIZE][FIELD_SIZE];
+    int next_gen[FIELD_SIZE][FIELD_SIZE];
 
     if (myrank == 0) {
 
         srandom(time(NULL + myrank));
 
-        for (i = 0; i < FIELD_SIZE+2; i++) {
-            for (j = 0; j <FIELD_SIZE+2; j++) {
-                field[i][j] = 0;
-            }
-        }
-
-        for (i = 1; i <= FIELD_SIZE; i++) {
-            for (j = 1; j <= FIELD_SIZE; j++) {
-                if (random() % 100 < 8) {
+        for (i = 0; i < FIELD_SIZE; i++) {
+            for (j = 0; j < FIELD_SIZE; j++) {
+                if (random() % 100 < 9) {
                     field[i][j] = 1;
                 }
                 else {
@@ -44,27 +39,32 @@ int main(int argc, char **argv) {
         }
 
         while (era < 100) {
-            for (i = 1; i <= FIELD_SIZE; i++) {
-                for (j = 1; j <= FIELD_SIZE; j++) {
+            for (i = 0; i < FIELD_SIZE; i++) {
+                for (j = 0; j < FIELD_SIZE; j++) {
+                    k = (FIELD_SIZE + i - 1) % FIELD_SIZE;
+                    l = (FIELD_SIZE + j - 1) % FIELD_SIZE;
+                    m = (i + 1) % FIELD_SIZE;
+                    n = (j + 1) % FIELD_SIZE;
 
-                    count = field[i-1][j-1] + field[i-1][j] + field[i-1][j+1]
-                          + field[i][j-1]   +               + field[i][j+1]
-                          + field[i+1][j-1] + field[i+1][j] + field[i+1][j+1];
+                    count = field[k][l]   +  field[k][j]   +  field[k][n]
+                          + field[i][l]   /*field[i][j]*/  +  field[i][n]
+                          + field[m][l]   + field[m][j]    +  field[m][n];
+
                     if (count == 3) {
-                        field[i][j] = 1;
+                        next_gen[i][j] = 1;
                     }
                     else if (count == 2) {
                         continue;
                     }
                     else {
-                        field[i][j] = 0;
+                        next_gen[i][j] = 0;
                     }
                 }
             }
 
             printf("\033[;H\033[2J");
-            for (i = 1; i <= FIELD_SIZE; i++) {
-                for (j = 1; j <= FIELD_SIZE; j++) {
+            for (i = 0; i < FIELD_SIZE; i++) {
+                for (j = 0; j < FIELD_SIZE; j++) {
                     if (field[i][j] == 1) {
                         printf("@");
                     }
@@ -74,8 +74,14 @@ int main(int argc, char **argv) {
                 }
                 printf("\n");
             }
+
             sleep(1);
             era++;
+            for (i = 0; i < FIELD_SIZE; i++) {
+                for (j = 0; j <FIELD_SIZE; j++) {
+                    field[i][j] = next_gen[i][j];
+                }
+            }
         }
     }
 

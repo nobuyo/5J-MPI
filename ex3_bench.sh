79e5a695fb698ca2506339a06c7d47caad2dcc07
nobuyo <world_fantasy_2017@live.jp>
2017-08-07 11:29:33 +0900

    add data

--- ex3_bench.sh
+++ ex3_bench.sh
@@ -23,25 +23,25 @@ d=`date "+%Y%m%d-%H%M%S"`
 for i in {1..30}; do
   res1=()
   res2=()
-  com=()
+  comm=()
 
   for j in {1..5}; do
     res=`mpirun -machinefile ~/mymachines -np $i ex3 | grep time | awk {'print $NF'} `
     com=`mpirun -machinefile ~/mymachines -np $i ex3 | grep comm | awk {'print $NF'} `
     res1+=`echo $res | sed -e 's/$/+/'`
     res2+=`echo $res | sed -e 's/$/, /'`
-    com+=`echo $com | sed -e 's/$/+/'`
+    comm+=`echo $com | sed -e 's/$/+/'`
   done
 
   ave=`echo "($res1 0) / 5.0" | bc -l`
-  ave_com=`echo "($com 0) / 5.0" | bc -l`
+  ave_com=`echo "($comm 0) / 5.0" | bc -l`
 
   echo "$i, $ave" >> data/ex3_average_$d.dat
   echo "$i, ${res2[@]}" >> data/ex3_raw_$d.dat
-  echo "$i, ${ave_com}" >> data/ex3_com_$d.dat
+  echo "$i, $ave_com" >> data/ex3_com_$d.dat
   unset res1[@]
   unset res2[@]
-  unset com[@]
+  unset comm[@]
 done
 
 # alias awk='/usr/bin/awk'
